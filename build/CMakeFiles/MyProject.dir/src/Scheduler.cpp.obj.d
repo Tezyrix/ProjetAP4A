03bfa5e9e808596080ff446f6871eff901da2f99
Tezyrix <nico2147@orange.fr>
2025-09-26 14:44:36 +0200

    removed /build

--- build/CMakeFiles/MyProject.dir/src/Scheduler.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/MyProject.dir/src/Scheduler.cpp.obj: \
- C:\Users\nico2\Documents\UTBM\AP4A\Seance1\src\Scheduler.cpp \
- C:/Users/nico2/Documents/UTBM/AP4A/Seance1/include/Scheduler.h
--- build/CMakeFiles/SystemeBadge.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for SystemeBadge.
-# This may be replaced when dependencies are built.
